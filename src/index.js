bcfb07cabbcc734c87756508eba65a7f47ab560f
Yevheniy Hordiyenko <johnylviv@gmail.com>
2024-07-31 14:16:45 +0300

    feat: Task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let absNumber = Math.abs(n);             /* convert to abs number*/
+  let str = absNumber.toString();          /* convert abs number to string */
+  str = str.split("").reverse().join("");  /* reverse string */
+
+  let revNumber = parseInt(str, 10);       /* convert string to Number */
+  return revNumber;
 }
